f86a010209c6f0e5a98ec8562c7d509c159290c3
matao12 <56285439+matao12@users.noreply.github.com>
2022-09-15 21:14:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o3YxU6hBS7bDJAUL1dYqek7gn_ck",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "5HDw0uF3BPG9Bl3xFtGjrm1YPd4dhfewBfU4qlPK5_g",
+      useTemplateId: "tnNjbQ96BUUXe6lAJ_SuBUrJq_MGVCfpKejEuB8hJPM",
       // 所在省份
       province: "河南",
       // 所在城市
